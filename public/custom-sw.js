2312583ff27b015c5e6394f81d3027d56f7e2934
duong120798 <duong120798@gmail.com>
2021-08-07 00:26:15 +0700

    push origin

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -13,6 +13,8 @@ self.addEventListener('push', event => {
       body: data.body,
       icon: data.icon,
       actions: data.actions,
+      image: data.icon,
+      badge: data.icon
     }
     event.waitUntil(
       // eslint-disable-next-line no-restricted-globals
@@ -21,4 +23,3 @@ self.addEventListener('push', event => {
   }
   
 })
-console.log(111333333)
