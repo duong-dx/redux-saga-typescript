dfb38f3dced730905a07821d1fbcfac61d408878
duongdx <dinh.xuan.duong@sun-asterisk.com>
2021-11-05 00:59:40 +0700

    commit chat

--- src/features/chats/chatSlide.ts
+++ src/features/chats/chatSlide.ts
@@ -32,10 +32,15 @@ export interface Message {
   message: string;
 }
 
+export interface MessageError {
+  conversation_id: number | string;
+  error: string;
+}
+
 export interface Conversation {
   users: User[];
   messages: Message[];
-  id: number | string;
+  id: number | string | null;
   title: string;
   description: string;
   sending: boolean;
@@ -49,17 +54,17 @@ export interface ListConversationState {
 }
 
 
-export interface ChatState {
-  messages: Message[];
-  sending: boolean;
-  error: string;
-}
+// export interface ChatState {
+//   messages: Message[];
+//   sending: boolean;
+//   error: string;
+// }
 
 
-const initialState: ChatState = {
-  sending: false,
+const initialState: ListConversationState = {
+  loading: false,
   error: '',
-  messages: [],
+  conversations: [],
 }
 
 export const chatSlice = createSlice({
@@ -70,30 +75,58 @@ export const chatSlice = createSlice({
       return state;
     },
     sendMessage(state, action: PayloadAction<Message>) {
-      state.sending = true
+      const conversation: Conversation | undefined = state.conversations.find(e => e.id === action.payload.conversation_id)
+      const conversationIndex = state.conversations.findIndex(e => e.id === action.payload.conversation_id)
+      const newConversation = {
+        id: conversation?.id ?? null,
+        title: conversation?.title ?? '',
+        description: conversation?.description ?? '',
+        users: conversation?.users ?? [],
+        messages: conversation?.messages ?? [],
+        sending: true,
+        error: '',
+      }
+
       console.log(state, 'chatSlide-sendMessage');
+      if (state.conversations.length === 0) {
+        state.conversations.push(newConversation);
+
+        return state;
+      } else if (state.conversations && conversationIndex >= 0) {
+        state.conversations[conversationIndex] = newConversation;
+
+        return state
+      }
 
       return state
     },
     
     sendMessageSuccess(state, action: PayloadAction<Message>) {
-      const newMessages = [
-        ...state.messages,
-        action.payload,
-      ]
-      state.messages = newMessages;
-      state.sending = false;
+      const newConversation = state.conversations.map((conversation) => {
+        if (conversation.id === action.payload.conversation_id) {
+          conversation.messages.push(action.payload)
+          conversation.sending = false
+        }
+        return conversation
+      })
 
-      console.log(newMessages, 'newMessages-sendMessageSuccess');
-      console.log(state.messages, state.sending, 'chatSlide-sendMessageSuccess');
+      state.conversations = newConversation;
+      console.log(newConversation, 'newConversation-sendMessageSuccess');
 
       return state
     },
 
-    sendMessageError(state, action: PayloadAction<string>) {
-      state.sending = false;
-      state.error = action.payload;
-      console.log(state, 'chatSlide-sendMessageError');
+    sendMessageError(state, action: PayloadAction<MessageError>) {
+      const newConversation = state.conversations.map((conversation) => {
+        if (conversation.id === action.payload.conversation_id) {
+          conversation.sending = false
+          conversation.error = action.payload.error
+        }
+        return conversation
+      })
+
+      state.conversations = newConversation;
+      console.log(newConversation, 'newConversation-sendMessageSuccess');
 
       return state
     }
